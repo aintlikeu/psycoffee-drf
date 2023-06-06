97019dfd990d1414084f3e6e747277472a93337c
aintlikeu <alex.goryashkiev@gmail.com>
2023-06-06 19:50:50 +0300

    Fix settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-
+    # other apps
+    'phonenumber_field',
+    # my apps
     'accounts',
     'api'
 ]
