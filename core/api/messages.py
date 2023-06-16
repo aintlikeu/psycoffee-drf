e98d8ec3878ecd91abe1fcfb364c95b6fa4afdcd
aintlikeu <alex.goryashkiev@gmail.com>
2023-06-16 15:28:18 +0300

    Move all messages to the separate file

--- core/api/messages.py
+++ core/api/messages.py
@@ -1,15 +1,13 @@
-from django.utils.translation import gettext_lazy as _
-
 # serializers
-SPOT_DOES_NOT_EXIST = _("Окно с таким spot_id не существует")
-SPOT_ALREADY_EXIST = _("Такое окно уже существует")
-SPOT_ALREADY_BOOKED = _("Окно уже забронировано")
-INCORRECT_SPOT_TIME = _("Неверная продолжительность сессии")
-INCORRECT_SPOT_DURATION = _("Возможные интервалы сессии 60, 90, 120 минут")
-INCORRECT_DATE_FORMAT = _("Некорректный формат даты")
-INCORRECT_DATE = _("Дата окна не может быть в прошлом")
-CUSTOMER_DOES_NOT_EXIST = _("Пользователя с таким customer_id не существует")
+SPOT_DOES_NOT_EXIST = "Окно с таким spot_id не существует"
+SPOT_ALREADY_EXIST = "Такое окно уже существует"
+SPOT_ALREADY_BOOKED = "Окно уже забронировано"
+INCORRECT_SPOT_TIME = "Неверная продолжительность сессии"
+INCORRECT_SPOT_DURATION = "Возможные интервалы сессии 60, 90, 120 минут"
+INCORRECT_DATE_FORMAT = "Некорректный формат даты"
+INCORRECT_DATE = "Дата окна не может быть в прошлом"
+CUSTOMER_DOES_NOT_EXIST = "Пользователя с таким customer_id не существует"
 
 # views
-NO_FIELDS_CUSTOMER_DATE = _("Не заданы поля customer_id, date")
-NO_FIELDS_CUSTOMER_DATE_TIME = _("Не заданы поля customer_id, date, time")
+NO_FIELDS_CUSTOMER_DATE = "Не заданы поля customer_id, date"
+NO_FIELDS_CUSTOMER_DATE_TIME = "Не заданы поля customer_id, date, time"
