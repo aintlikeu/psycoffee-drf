dd798f7437d577a3c20655e0fc74863162a01b75
Mikhail Davydenkov <davydenkovmiu@laspace.ru>
2023-08-16 10:38:01 +0300

    fix username to proper render admin pages

--- core/core/urls.py
+++ core/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/', include('accounts.urls')),
-    # re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf'))
+    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
 
     path("api/auth/", include("dj_rest_auth.urls")),  # endpoints provided by dj-rest-auth
     path("api/social/login/", include("nextjsauth.urls")),  # our own views
